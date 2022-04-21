70007d33d8ece722387175b30e1448ae959688d8
Cherie Allen <cherie@Cheries-MacBook-Air.local>
2022-04-21 11:57:53 -0400

    chicken and egg

--- loops.js
+++ loops.js
@@ -1,9 +1,18 @@
-const alumni = ['Ashley', 'Peter','Sebas', 'Zhuopu','Aurelie','Matt',]
+// const alumni = ['Ashley', 'Peter','Sebas', 'Zhuopu','Aurelie','Matt',]
 
-for( let alum of alumni) {
-    console.log(alum)
-}
+// for( let alum of alumni) {
+//     console.log(alum)
+// }
 
-for (let i= 0; i<alumni.length; i++){
-    console.log(i,alumni[i])
-}
+// for (let i= 0; i<alumni.length; i++){
+//     console.log(i,alumni[i])
+// }
+
+console.log ('This comes first.')
+
+setTimeout(function() {
+    console.log ('this is in the middle...')
+}, 0)
+
+console.log ('This is the end')
+
